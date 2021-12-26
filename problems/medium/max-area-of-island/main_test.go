8b4ea5ca7e1c8b6fea7cb16577be81d92ceb8e61
bridennis <5626164+bridennis@users.noreply.github.com>
2021-12-26 21:07:14 +0300

    Adds medium solution: max-area-of-island

---
+++ problems/medium/max-area-of-island/main_test.go
@@ -0,0 +1,36 @@
+package medium_max_area_of_island
+
+import "testing"
+
+func Test_maxAreaOfIsland(t *testing.T) {
+	type args struct {
+		grid [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Max area #1",
+			args: struct{ grid [][]int }{grid: [][]int{
+				{0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+				{0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+				{0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0},
+			}},
+			want: 6,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxAreaOfIsland(tt.args.grid); got != tt.want {
+				t.Errorf("maxAreaOfIsland() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
